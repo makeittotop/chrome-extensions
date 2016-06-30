fe4178e8a3d8563d506925f9fab38042ad424cad
abhishek pareek <mail@abhishekpareek.io>
2016-06-30 16:35:12 +0400

    HT-Unblocker : Working version

--- unblock-ht/background.js
+++ unblock-ht/background.js
@@ -4,20 +4,10 @@
  * @see http://developer.chrome.com/apps/app.runtime.html
  * @see http://developer.chrome.com/apps/app.window.html
  */
-
-chrome.app.runtime.onLaunched.addListener(function(launchData) {
-  chrome.app.window.create(
-    'index.html',
-    {
-      id: 'mainWindow',
-      bounds: {width: 800, height: 600}
-    }
-  );
-});
-
-chrome.tabs.executeScript(null, {
-  code: 'var config = ' + JSON.stringify("foobar")
-  }, function() {
-    console.log("foo");
-    chrome.tabs.executeScript(null, {file: 'main.js'});
+chrome.browserAction.onClicked.addListener(function(tab) {
+  /*chrome.tabs.executeScript({
+    code: 'document.body.style.backgroundColor="red"'
+  });*/
+  console.log("Unblocking " + tab.url);
+  chrome.tabs.executeScript(null, {file: "custom.js"});
 });
---
+++ unblock-ht/custom.js
@@ -0,0 +1,2 @@
+document.getElementById("div_storyContent").style.display="block";
+document.getElementsByClassName("adblocker-container")[0].style.display="none";
